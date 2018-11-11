b29977aa510d82d7d189143aa24d9d7a8e2fbb7d
Janacek <thomas.epitech@gmail.com>
2018-11-11 11:13:12 +0100

    Trigger Manager

---
+++ GameDevPartyJam14/Assets/Scripts/Trigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+	public int EventNumber;
+
+	void Start()
+	{
+		tm = GameObject.Find("TriggerManager").GetComponent<TriggerManager>();
+	}
+
+	void Update()
+	{
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		tm.PlayEvent(EventNumber);
+		EventNumber = -1;
+	}
+
+	TriggerManager tm;
+}
---
+++ GameDevPartyJam14/Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TriggerManager : MonoBehaviour
+{
+	public List<AudioClip> clips;
+	public List<string> texts;
+	public List<float> times;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		sousTitres = GameObject.Find("SousTitres").transform.Find("Canvas").Find("SousTitreText").GetComponent<TextMeshProUGUI>();
+		sousTitres.enabled = false;
+	}
+
+	void Update()
+	{
+		if (timer > 0.0f)
+		{
+			timer -= Time.deltaTime;
+		}
+		else
+		{
+			sousTitres.enabled = false;
+		}
+	}
+
+	public void PlayEvent(int number)
+	{
+		if (number != -1)
+		{
+			audioSource.PlayOneShot(clips[number]);
+			sousTitres.text = texts[number];
+			timer = times[number];
+			sousTitres.enabled = true;
+		}
+	}
+
+	float timer;
+
+	TextMeshProUGUI sousTitres;
+	AudioSource audioSource;
+}
